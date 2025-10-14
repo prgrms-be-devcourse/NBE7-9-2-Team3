d3076d7a2f182273db60f407d849e748215cae89
[BE9]도석환 <do04080@gmail.com>
2025-10-14 16:23:30 +0900

    feat: exception 추가

---
+++ backend/src/main/java/org/example/backend/global/exception/ServiceException.java
@@ -0,0 +1,34 @@
+package org.example.backend.global.exception;
+
+import org.example.backend.global.rsdata.RsData;
+import org.springframework.http.HttpStatus;
+
+public class ServiceException extends RuntimeException {
+
+    private final String resultCode;
+    private final String msg;
+    private final HttpStatus httpStatus;
+
+    public ServiceException(String resultCode, String msg, HttpStatus httpStatus) {
+        super("%s : %s".formatted(resultCode, msg));
+        this.resultCode = resultCode;
+        this.msg = msg;
+        this.httpStatus = httpStatus;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public RsData getRsData() {
+        return new RsData(resultCode, msg);
+    }
+}
