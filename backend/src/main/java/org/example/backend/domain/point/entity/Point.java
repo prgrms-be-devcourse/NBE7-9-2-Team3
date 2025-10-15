4a3e92a187fb58726ae527ac30c787bcce0f6a7a
xoxoisme <kxoxo3333@gmail.com>
2025-10-15 15:43:27 +0900

    feat: 거래기능 entity 추가

---
+++ backend/src/main/java/org/example/backend/domain/point/entity/Point.java
@@ -0,0 +1,49 @@
+package org.example.backend.domain.point.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.backend.domain.member.entity.Member;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "point_log")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Point {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long logId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private TransactionType type = TransactionType.CHARGE;
+
+    @Column(nullable = false)
+    private Long points = 0L;
+
+    @Column(nullable = false)
+    private Long afterPoint = 0L;
+
+    @Column(nullable = false)
+    private LocalDateTime createDate;
+
+    // 내역 생성
+    public static Point create(Member member, long amount, long afterPoint) {
+        Point point = new Point();
+        point.member = member;
+        point.type = TransactionType.CHARGE;
+        point.points = amount;
+        point.afterPoint = afterPoint;
+        point.createDate = LocalDateTime.now();
+        return point;
+    }
+}
