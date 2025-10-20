b49ab1aaad45d8ba8f34bbde1574d6baa8aa2bcd
qivvoon <gangjiwon70@gmail.com>
2025-10-20 15:40:14 +0900

    refactor: 어항 목록 리랜더링 안되는 문제 수정

--- backend/src/main/java/org/example/backend/domain/fish/dto/FishResponseDto.java
+++ backend/src/main/java/org/example/backend/domain/fish/dto/FishResponseDto.java
@@ -3,12 +3,14 @@ package org.example.backend.domain.fish.dto;
 import org.example.backend.domain.fish.entity.Fish;
 
 public record FishListResponseDto(
+    Long fishId,
     String fishSpecies,
     String fishName
 ) {
 
   public FishListResponseDto(Fish fish) {
     this(
+        fish.getId(),
         fish.getSpecies(),
         fish.getName()
     );
