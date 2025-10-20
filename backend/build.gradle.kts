adbc11a1526c6ef336b1417d6327f0a068eed3da
xoxoisme <kxoxo3333@gmail.com>
2025-10-20 16:04:49 +0900

    feat: WebSocket 및 Jackson Databind 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
 }
 
 tasks.withType<Test> {
