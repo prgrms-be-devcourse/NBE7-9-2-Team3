cea4f4bfec4fe1c84f3cea636838cf5add911a33
qivvoon <gangjiwon70@gmail.com>
2025-10-21 09:46:47 +0900

    refactor: aquarium, fish requestDto에 @valid 적용

--- backend/src/main/java/org/example/backend/domain/aquarium/dto/AquariumCreateRequestDto.java
+++ backend/src/main/java/org/example/backend/domain/aquarium/dto/AquariumCreateRequestDto.java
@@ -1,5 +1,12 @@
 package org.example.backend.domain.aquarium.dto;
 
-public record AquariumCreateRequestDto(String aquariumName) {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record AquariumCreateRequestDto(
+    @NotNull
+    @Size(min = 1, max = 50)
+    String aquariumName
+) {
 
 }
