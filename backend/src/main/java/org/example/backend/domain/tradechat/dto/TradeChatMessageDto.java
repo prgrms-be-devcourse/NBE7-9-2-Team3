83ad8858aed90af9529d9f17d6c7111a4286069e
xoxoisme <kxoxo3333@gmail.com>
2025-10-22 12:45:25 +0900

    refactor: 채팅방에 닉네임 추가하기 위해 구조 변경

--- backend/src/main/java/org/example/backend/domain/tradechat/dto/TradeChatMessageDto.java
+++ backend/src/main/java/org/example/backend/domain/tradechat/dto/TradeChatMessageDto.java
@@ -5,13 +5,17 @@ import org.example.backend.domain.tradechat.entity.TradeChatMessage;
 import java.time.LocalDateTime;
 
 public record TradeChatMessageDto(
+        Long messageId,
         Long senderId,
+        String senderNickname,
         String content,
         LocalDateTime sendDate
 ) {
     public static TradeChatMessageDto from(TradeChatMessage message) {
         return new TradeChatMessageDto(
+                message.getId(),
                 message.getSender().getMemberId(),
+                message.getSender().getNickname(),
                 message.getContent(),
                 message.getSendDate()
         );
